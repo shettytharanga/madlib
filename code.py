17ef7720b355c91a632ae5e43dd90e201ebd64f8
shettytharanga <shettytharanga034@gmail.com>
2023-08-25 20:59:07 +0530

    added the madlib code

---
+++ code.py
@@ -0,0 +1,6 @@
+noun = input("noun : ")
+verb1 = input("verb1 : ")
+verb2 = input("verb2 : ")
+famous_person = input("famous_person : ")
+
+print(f"hello fellas i am {noun}. wassup. so i was {verb1} and think {verb2} with {famous_person}")
